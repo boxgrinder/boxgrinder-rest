28dd28c10ee68e75af1cbff1c582649b06b69375
Marek Goldmann <marek.goldmann@gmail.com>
2011-02-24 10:20:47 +0100

    Default Rails app with TB template.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoxgrinderRest::Application.config.secret_token = 'cbca630c2902334911caa6414de802ea9383ee03ab6738f73ec01785a67f6479d5992ccdd315c05ee8cd0aa1f20bc90efc675259456a8513780eeb4fb6812f67'
---
+++ lib/tasks/torquebox.rake
@@ -0,0 +1,16 @@
+
+begin
+  require 'org.torquebox.rake-support'
+rescue LoadError => ex
+  puts "Failed to load the TorqueBox rake gem (org.torquebox.rake-support). Make sure it is available in your environment."
+end
+
+# Patch db:load_config to make sure activerecord-jdbc-adapter gets loaded
+namespace :db do
+  task :load_config => :rails_env do
+    require 'active_record'
+    require 'activerecord-jdbc-adapter'
+    ActiveRecord::Base.configurations = Rails::Application.config.database_configuration
+  end
+end
+
